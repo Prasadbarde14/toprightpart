c6b95fac186eba7b57e53b9ab55bef17795cf677
Prasad.barde14 <prasad.barde@gaiansolutions.com>
2023-12-28 16:08:51 +0530

    top right code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="parent">
+        <div class="video-container video-container2">
+         <div class="video-scaler">
+          <iframe src="https://dbaas.netlify.app/" frameborder="0"></iframe>
+         </div>
+        </div>
+    </div>
+    </div>
+
+    
+  );
+}
+
+export default App;
